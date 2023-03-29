62d6fd43937100d2cbac2d5cacd0cf26d40f6b63
Matt Brouwer <notjonbonjovi@gmail.com>
2023-03-28 22:52:40 -0400

    loop lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+
+function writeCards(name, event) {
+ let messages = []
+  for (let i = 0; i < name.length; i++)
+    messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    return messages
+  }
+
+  function countDown(positiveInteger) {
+    while(positiveInteger >= 0) {
+    console.log(positiveInteger)
+    positiveInteger--;}
+  }
+  
